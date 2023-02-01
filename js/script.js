f0707b4119e12a24b4939c9c7afc0047521f68a5
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2023-02-01 15:37:06 +0700

    lesson 55

--- js/script.js
+++ js/script.js
@@ -477,5 +477,58 @@ window.addEventListener('DOMContentLoaded', () => {
             closeModal();
         }, 4000)
     }
+});
 
+//Promise
+
+// console.log('Запрос данных....');
+//
+// const req = new Promise(function (resolve, reject) {
+//     setTimeout(() => {
+//         console.log("Подготовка данных...");
+//
+//         const product = {
+//             name: 'TV',
+//             price: 20000
+//         };
+//
+//         resolve(product);
+//     }, 2000);
+// });
+//
+// req.then((product) => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             product.status = 'order';
+//             resolve(product);
+//         }, 2000);
+//     });
+// }).then(data => {
+//     data.modify = true;
+//     return data;
+// }).then((data) => {
+//     console.log(data)
+// }).catch(() => {
+//     console.error('Произошла ошибка')
+// }).finally(() => {
+//     console.log('Finaly')
+// });
+
+
+//Promise: all race
+const test = time => {
+    return new Promise(resolve => {
+        setTimeout(() => resolve(), time)
+    });
+};
+
+// test(1000).then(() => console.log('1000 ms'));
+// test(2000).then(() => console.log('2000 ms'));
+
+// Promise.all([test(1000), test(2000)]).then(() => {
+//     console.log('All')
+// });
+
+Promise.race([test(1000), test(2000)]).then(() => {
+    console.log('All')
 });
