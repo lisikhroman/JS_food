102ca1aaa2d474bc4a0e10c74c86098d8ad5566c
DESKTOP-CUGMPBP\79234 <Roman.Lisikh@indusoft.ru>
2023-02-02 15:30:03 +0700

    lesson 60

--- js/script.js
+++ js/script.js
@@ -330,15 +330,15 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    const getResource = async (url) => {
-        const res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-        }
-
-        return await res.json();
-    };
+    // const getResource = async (url) => {
+    //     const res = await fetch(url);
+    //
+    //     if (!res.ok) {
+    //         throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    //     }
+    //
+    //     return await res.json();
+    // };
 
     // getResource('http://localhost:3000/menu')
     //     .then(data => {
@@ -346,7 +346,16 @@ window.addEventListener('DOMContentLoaded', () => {
     //             new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
     //         })
     //     });
-    
+
+    //Axios
+
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            })
+        });
+
     // new MenuCard(
     //     "img/tabs/vegy.jpg",
     //     "vegy",
